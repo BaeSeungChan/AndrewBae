62e1057fefb80d164b579bdd372f51899c5d6aa0
kosta <kosta@192.168.0.104>
2017-09-26 16:24:18 +0900

    MemberVO에 id 추가(setter, getter)

--- gitTestAndrew/src/com/movie/model/MemberVO.java
+++ gitTestAndrew/src/com/movie/model/MemberVO.java
@@ -1,5 +1,15 @@
 package com.movie.model;
 
 public class MemberVO {
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
 	
 }
